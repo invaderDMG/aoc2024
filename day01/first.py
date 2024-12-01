b9b6fdfa49722af22d7f4b621b41e9443d76c2d2
jdlcgarcia <jcruzgar@gmail.com>
2024-12-01 20:00:21 +0100

    included in a main function

--- day01/first.py
+++ day01/first.py
@@ -1,15 +1,18 @@
 # Day 01
 from shared.utils import read_input, transform_input
 
-input_data = read_input("day01/input.txt")
+def main():
+    input_data = read_input("day01/input.txt")
 
-first_list, second_list = transform_input(input_data)
+    first_list, second_list = transform_input(input_data)
 
-first_list.sort()
-second_list.sort()
+    first_list.sort()
+    second_list.sort()
 
-total_difference = 0
-for a, b in zip(first_list, second_list):
-    total_difference += abs(a - b)
+    total_difference = 0
+    for a, b in zip(first_list, second_list):
+        total_difference += abs(a - b)
 
-print("The sum total of the absolute differences is:", total_difference)
+    print("The sum total of the absolute differences is:", total_difference)
+
+main()
