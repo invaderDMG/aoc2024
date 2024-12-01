5afe4c85cc50cd0440f95be6c0131645c0505dc1
jdlcgarcia <jcruzgar@gmail.com>
2024-12-01 12:56:33 +0100

    solución del primer apartado

--- day01/main.py
+++ day01/main.py
@@ -1,5 +1,18 @@
 # Day 01
-from shared.utils import read_input
+from shared.utils import read_input, transform_input
 
+# Leer la entrada del archivo
 input_data = read_input("day01/input.txt")
-print(input_data)
+
+# Transformar la salida de parse_input
+first_list, second_list = transform_input(input_data)
+
+first_list.sort()
+second_list.sort()
+
+# Calcular la suma de las diferencias absolutas
+total_difference = 0
+for a, b in zip(first_list, second_list):
+    total_difference += abs(a - b)
+
+print("La suma total de las diferencias absolutas es:", total_difference)
