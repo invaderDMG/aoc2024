08ee6a12b49794ad93caa67d6681eb263eea7add
jdlcgarcia <jcruzgar@gmail.com>
2024-12-01 13:22:46 +0100

    solución del segundo ejercicio

---
+++ day01/second.py
@@ -0,0 +1,28 @@
+# Day 01
+from shared.utils import read_input, transform_input
+
+def main():
+    # Leer la entrada del archivo
+    input_data = read_input("day01/input.txt")
+
+    # Transformar la salida de parse_input
+    first_list, second_list = transform_input(input_data)
+
+    total_similarity = 0
+    similarity_map = {}
+    for a in first_list:
+        similarity_map[a] = findSimilarity(a, second_list)
+        total_similarity += a*similarity_map[a]
+
+    print("Total similarity is ", total_similarity)
+
+
+def findSimilarity(needle, haystack):
+    similarity = 0
+    for a in haystack:
+         if a == needle:
+             similarity += 1
+
+    return similarity
+
+main()
