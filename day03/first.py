6a4550f1ec4e0d7afd0052d824a2e329d8331379
jdlcgarcia <jcruzgar@gmail.com>
2024-12-03 12:41:32 +0100

    refactored first solution to reuse some/organize

--- day03/first.py
+++ day03/first.py
@@ -3,7 +3,6 @@
 
 from shared.utils import read_input
 
-
 def main():
     input_data = read_input("day03/input.txt")
     foundOperations = []
@@ -13,22 +12,7 @@ def main():
         i = 0
         while i < len(corruptedMemory):
             cursor = i+4
-            if (corruptedMemory[i:cursor] == "mul("):
-                number1 = findNumber(corruptedMemory, cursor)
-                if (number1 == ''):
-                    i+=cursor
-                else:
-                    cursor += len(number1)
-                    comma = findSymbol(corruptedMemory, cursor, ",")
-                    if (comma != ''):
-                        cursor += 1
-                        number2 = findNumber(corruptedMemory, cursor)
-                        if (number2 != ''):
-                            cursor += len(number2)
-                            closingParenthesis = findSymbol(corruptedMemory, cursor, ")")
-                            if (closingParenthesis != ''):
-                                cursor += 1
-                                foundOperations.append([corruptedMemory[i:cursor], number1, number2])
+            findMul(i, cursor, corruptedMemory, foundOperations)
             i+=1
     
     total = 0
@@ -36,6 +20,24 @@ def main():
         total += int(foundOperations[i][1]) * int(foundOperations[i][2])
 
     print("total of found operations is", total)
+
+def findMul(i, cursor, corruptedMemory, foundOperations):
+    if (corruptedMemory[i:cursor] == "mul("):
+        number1 = findNumber(corruptedMemory, cursor)
+        if (number1 == ''):
+            i+=cursor
+        else:
+            cursor += len(number1)
+            comma = findSymbol(corruptedMemory, cursor, ",")
+            if (comma != ''):
+                cursor += 1
+                number2 = findNumber(corruptedMemory, cursor)
+                if (number2 != ''):
+                    cursor += len(number2)
+                    closingParenthesis = findSymbol(corruptedMemory, cursor, ")")
+                    if (closingParenthesis != ''):
+                        cursor += 1
+                        foundOperations.append([corruptedMemory[i:cursor], number1, number2])
             
 def findNumber(corruptedMemory, index):
     number = ''
