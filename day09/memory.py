f271e9a1af488bd038314ec01a479c1f4c4b3821
jdlcgarcia <jcruzgar@gmail.com>
2024-12-16 02:48:46 +0100

    non working second part, but good enough for me

--- day09/memory.py
+++ day09/memory.py
@@ -5,9 +5,8 @@ from day09.memoryCell import MemoryCell
 
 class Memory:
     def __init__(self):
-        self.cell = []
         self.files: List[File] = []
-        self.free = []
+        self.free: List[File] = []
         self.memoryMap: List[MemoryCell] = []
         self.totalFreeMemory = 0
         
@@ -26,7 +25,8 @@ class Memory:
         
         
     def addFreeSpace(self, size: int):
-        self.free.append(size)
+        file = File(-1, size)
+        self.free.append(file)
         for i in range(size):
             self.memoryMap.append(MemoryCell(len(self.memoryMap), '.'))
         self.totalFreeMemory += size
@@ -35,7 +35,6 @@ class Memory:
         lastFileCursor = self.getLastFileCursor()
         lastFile = self.files[int(self.memoryMap[lastFileCursor].value)]
         self.clear(lastFileCursor - lastFile.size +1, lastFileCursor)
-        print("compacting file on [",lastFileCursor - lastFile.size +1, ",", lastFileCursor,"]", self.memoryMap[lastFileCursor].value)
         
         cursor = 0
         remainingFile = str(lastFile.id)*lastFile.size
@@ -69,4 +68,88 @@ class Memory:
                 checksum += index * int(memoryCell.value)
                 # print(index, "*", int(memoryCell.value))
                 
+        return checksum
+    
+class MemoryBlocks:
+    def __init__(self):
+        self.blocks: List[File] = []
+        self.lastInsertedId = -1
+        
+    def __repr__(self):
+        representation = ''
+        for block in self.blocks:
+            representation += '['+str(block.id)+']'
+        return representation
+    
+    def memoryMap(self):
+        representation = ''
+        for block in self.blocks:
+            if(block.id > -1):
+                representation += str(block.id)*block.size
+            else:
+                representation += '.'*block.size
+        return representation
+    
+    def addFile(self, size: int):
+        self.lastInsertedId+=1
+        file = File(self.lastInsertedId, size)
+        self.addBlock(file)
+        
+        
+    def addFreeSpace(self, size: int):
+        freeSpace = File(-1, size)
+        self.addBlock(freeSpace)
+    
+    def addBlock(self, file: File):
+        self.blocks.append(file)
+        
+    def blockCompact(self):
+        lastElementId = self.lastInsertedId
+        while (lastElementId > 0):
+            print(self)
+            print("compacting",lastElementId)
+            lastFileIndex = self.findFileIndexFromLastOne(lastElementId)
+            if (lastFileIndex != -1):
+                fittingCursor = self.findFittingSpace(lastFileIndex)
+                if (fittingCursor != -1 and fittingCursor < lastFileIndex):
+                    self.fitInEmptySpace(fittingCursor, lastFileIndex)
+            lastElementId -= 1
+            
+    def findFileIndexFromLastOne(self, elementId):
+        for index, element in reversed(list(enumerate(self.blocks))):
+            if (element.id == elementId):
+                return index
+        return -1
+            
+    def fitInEmptySpace(self, fittingCursor, lastFileIndex):
+        
+        file = File(self.blocks[lastFileIndex].id, self.blocks[lastFileIndex].size)
+        self.blocks[lastFileIndex].id = -1
+        if (self.blocks[fittingCursor].size == file.size):
+            self.blocks[fittingCursor].id = file.id
+        else:
+            self.blocks[fittingCursor].size -= file.size
+            self.blocks.insert(fittingCursor, file)
+             
+    def findFittingSpace(self, id: int):
+        cursor = 0
+        while (cursor < len(self.blocks)):
+            if (self.blocks[cursor].id == -1 and self.blocks[cursor].size >= self.blocks[id].size):
+                return cursor
+            cursor+=1
+            
+        return -1
+    
+    def checksum(self):
+        checksum = 0
+        indexCounter = 0
+        while (len(self.blocks) > 0):
+            if(self.blocks[0].id >= 0):
+                checksum += indexCounter * self.blocks[0].id
+                print(indexCounter,"*",self.blocks[0].id)
+            self.blocks[0].size-=1
+            if (self.blocks[0].size <= 0):
+                del(self.blocks[0])
+            indexCounter+=1
+            
         return checksum
--- day09/second.py
+++ day09/second.py
@@ -1,1 +1,25 @@
 # Day 09 - Second puzzle
+from day09.memory import MemoryBlocks
+from shared.utils import read_input
+
+def main():
+    row = read_input("day09/test_input.txt")[0]
+    memory = MemoryBlocks()
+    
+    for i in range(len(row)):
+        if i % 2 == 0:
+            memory.addFile(int(row[i]))
+        else:
+            memory.addFreeSpace(int(row[i]))
+            
+    # print(memory)
+    # print(memory.memoryMap())
+    
+    print("starts compacting")
+    memory.blockCompact()    
+    print("finished compacting")
+    
+    print(memory.checksum())
+        
+
+main() 
